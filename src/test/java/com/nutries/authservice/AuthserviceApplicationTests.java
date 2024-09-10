76b9e56416fee6bbc1588d71ca6401bd1a72a1dd
Vyacheslavvvv <galin0ff@yandex.ru>
2024-09-10 11:13:54 +0300

    init

---
+++ src/test/java/com/nutries/authservice/AuthserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutries.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
